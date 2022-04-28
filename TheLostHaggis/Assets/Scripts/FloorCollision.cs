3caa15b5d15e37e793701ea130007952abf60d8a
lam12602 <79139004+lam12602@users.noreply.github.com>
2022-04-28 14:50:02 +0100

    Added collision checker and basic platforms

---
+++ TheLostHaggis/Assets/Scripts/FloorCollision.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorCollision : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //thing we ran into
+        GameObject othherObject = collision.gameObject;
+
+        //gets the name of the object
+        string otherName = othherObject.name;
+
+        //check the tag on the object
+        string otherTag = othherObject.tag;
+
+        //we can get sorting layer of the obeject
+        int layer = othherObject.layer;
+
+        //we canm check if a script is on the object
+        //getting a componetnt
+        SpriteRenderer spriteRenderer = othherObject.GetComponent<SpriteRenderer>();
+
+        Debug.Log("Collison");
+        Debug.Log("Name" + otherName);
+        Debug.Log("Tag" + otherTag);
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.color = Color.green;
+        }
+
+
+    }
+}
