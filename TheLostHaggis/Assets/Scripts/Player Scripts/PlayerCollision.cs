d95aa72e3b4fc8f66d6f7af38a10f77d122fd79d
Mbeveridge4 <L000225343@fife.ac.uk>
2022-05-11 12:34:56 +0100

    Initial setup of Timeline Cutscene process

--- TheLostHaggis/Assets/Scripts/Player Scripts/PlayerCollision.cs
+++ TheLostHaggis/Assets/Scripts/Player Scripts/PlayerCollision.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerCollision : MonoBehaviour
 {
 
     public Vector3 originalPos;
- 
+    private gamemaster gm;
 
     private void Awake()
     {
@@ -15,11 +16,14 @@ public class PlayerCollision : MonoBehaviour
     }
     private void OnCollisionEnter2D (Collision2D other)
     {
-        
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<gamemaster>();
+
         if (other.gameObject.tag == "Enemy")
         {
             Debug.Log("TESTER");
-            gameObject.transform.position = originalPos;
+          //  gameObject.transform.position = originalPos;
+            transform.position = gm.LastCheckpointPos;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
 
     }
